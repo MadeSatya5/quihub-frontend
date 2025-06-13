24f6886cdab82cea3e04fb4b915e1a650ada3f19
MadeSatya5 <madesatya505@gmail.com>
2025-06-14 03:54:25 +0700

    fix: production bug

--- src/app/(auth)/login/hooks/useLoginMutation.ts
+++ src/app/(auth)/login/hooks/useLoginMutation.ts
@@ -2,11 +2,11 @@ import { setToken } from "@/lib/cookies";
 import { LoginRequest, LoginResponse } from "@/types/auth";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 export const useLoginMutation = () => {
-  const router = useRouter();
+  // const router = useRouter();
   const { mutate, isPending } = useMutation({
     mutationFn: async (data: LoginRequest) => {
       const response = await axios.post<LoginResponse>(
@@ -23,7 +23,8 @@ export const useLoginMutation = () => {
     },
     onSuccess: () => {
       toast.success("Anda berhasil login!!");
-      router.push("/dashboard");
+      window.location.href = "/dashboard";
+      // router.push("/dashboard");
     },
     onError: () => {
       toast.error("Email atau kata sandi salah, silahkan coba lagi");
