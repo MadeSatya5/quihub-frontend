570ed248cb80cd48d474bff8f17b91d35245cb12
MadeSatya5 <madesatya505@gmail.com>
2025-06-22 21:13:34 +0700

    fix: delete zombie code

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
 function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <QueryClientProvider client={queryClient}>
+  return (
+    <QueryClientProvider client={queryClient}>
       <Toaster position="top-center" />
       {children}
     </QueryClientProvider>
-    )
+  );
 }
 
-export default Providers
+export default Providers;
